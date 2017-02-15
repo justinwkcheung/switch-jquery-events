3f7c5b7d50fc37dbee34ab9ac0ff18f022256443
Justin Cheung <justinwkcheung@github.com>
2017-02-15 16:25:44 -0500

    Change background color when light switches on or off and display message

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
+function switchUp() {
+  if ($('.switch button').attr('class') === 'on') {
+    $('.switch button').attr('class', 'off')
+  }
+  else {
+    $('.switch button').attr('class', 'on')
+  }
+};
+
+function switchLight() {
+  if ($('.switch button').attr('class') === 'on') {
+    $('body').css('background-color', 'white')
+    $('.status').html("It's so bright in here!").css('color', 'black')
+  }
+  else {
+    $('body').css('background-color', 'black')
+    $('.status').html('Hey, who turned off the lights?').css('color', 'white')
+  }
+};
 
 
+$(document).on('ready', function() {
+
+  $('.switch button').on('click', switchUp);
+  $('.switch button').on('click', switchLight);
+
+});
